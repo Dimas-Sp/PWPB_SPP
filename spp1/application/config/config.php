4b879bf738ee3de1acd304c22c1ee25ab58d992a
unknown <dimas302putra@gmail.com>
2021-10-21 11:43:31 +0700

    PWPB_1

--- spp1/application/config/config.php
+++ spp1/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/spp/';
+$config['base_url'] = 'http://localhost/spp1/';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,3 +521,4 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
