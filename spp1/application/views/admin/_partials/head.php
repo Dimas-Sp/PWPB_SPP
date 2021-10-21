2b37566ac428e09dcff8942146d72e032d9ef1b3
unknown <dimas302putra@gmail.com>
2021-10-21 11:53:39 +0700

    PWPB_SPP

---
+++ spp1/application/views/admin/_partials/head.php
@@ -0,0 +1,16 @@
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="img/logo/logo.png" rel="icon">
+  <title><?php echo SITE_NAME .": " .ucfirst($this->uri->segment(1)) ." - " .ucfirst($this->uri->segment(2)) ?></title>
+  <link href="<?php echo base_url('assets/RuangAdmin-master/vendor/fontawesome-free/css/all.min.css');?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url('assets/RuangAdmin-master/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url('assets/RuangAdmin-master/css/ruang-admin.min.css'); ?>" rel="stylesheet">
+
+  <!-- Page level Plugin CSS -->
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.3/datatables.min.css"/>
+
+</head>
---
+++ spp1/application/views/admin/_partials/js.php
@@ -0,0 +1,7 @@
+    <script src="<?php echo base_url('assets/RuangAdmin-master/vendor/jquery/jquery.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/RuangAdmin-master/vendor/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/RuangAdmin-master/vendor/jquery-easing/jquery.easing.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/RuangAdmin-master/js/ruang-admin.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/RuangAdmin-master/vendor/chart.js/Chart.min.js'); ?>"></script>
+  <script src="<?php echo base_url('assets/RuangAdmin-master/js/demo/chart-area-demo.js'); ?>"></script>  
+  <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.3/datatables.min.js"></script>
